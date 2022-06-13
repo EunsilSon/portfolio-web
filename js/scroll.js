84f2107fc57764b2123738a24a247a16d51f5b21
EunsilSon <eunsil1023@gmail.com>
2022-06-13 17:25:56 +0900

    파일 경로 수정

--- js/scroll.js
+++ js/scroll.js
@@ -10,16 +10,3 @@ if (jQuery(window).width() > 0) {
     return false;
   });
 }
-
-/* 해당위치로 스크롤링 */
-$(function(){
-  var headerHight = 80;
-  $("a[href^=#]").click(function() {
-    var speed = 500; // 스크롤 속도
-    var href= $(this).attr("href");
-    var target = $(href == "#" || href == "" ? "div" : href);
-    var position = target.offset().top - headerHight; // 헤더 높이 만큼 위치를 조절함
-    $("html, body").animate({scrollTop : position}, speed, 'swing');
-    return false;
-    });
-});
